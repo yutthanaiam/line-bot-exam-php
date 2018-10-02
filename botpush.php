e90fcae88ffb642a3ad3db11a546c62d837ceaf9
yutthanaiam <43771083+yutthanaiam@users.noreply.github.com>
2018-10-02 13:02:32 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -6,9 +6,9 @@ require "vendor/autoload.php";
 
 $access_token = '5wkGqtW7/6dOw++pcRZZdR8wIBeZ8n4z/S+P0oV0wggFUp30P0zJh3s587cpc8VFriavsvRziAKmdJd6YWGrmphZTrnL/NpJzMVmOw72m5WiCWgoxg5yFrwh+NgtswDhMYVJquHFqpS//RAlzdGgXwdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'f1205ae43da6353e326f4ce53c6611b9';
 
-$pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
+$pushID = 'U947ea2bf8af0a5ce2b3af46a6a52239d';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
