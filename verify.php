16df14e0bec2621440ebd7d581f5c3a4f4b35f02
yutthanaiam <43771083+yutthanaiam@users.noreply.github.com>
2018-10-02 14:24:21 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $access_token = 'Mi0y1Y3KsSTTfEh5ddH11wsnax89sd5y+4y/NoF+hyG15GExwZw80HzqlaXM7XUeriavsvRziAKmdJd6YWGrmphZTrnL/NpJzMVmOw72m5XY4Mw0hnAQ+46vSCkHIxxZZ2kzn/D5VoZ+5D8OzxT5hwdB04t89/1O/w1cDnyilFU=';
 
 
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/v2/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
